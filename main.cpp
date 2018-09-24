0875d71ea950e9bb19c28d166eca2916fbd72211
Xander_ <alessandro.cevoli2@studio.unibo.it>
2018-09-24 17:26:06 +0200

    refactored and generalized CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -28,11 +28,11 @@ const string nbayes_folder = "../res/bayes";
 
 void showHelper(void) {
 
-    cout << "-o == Run Observation process on the RasPi Camera";
+    cout << "-o [== Run Observation process on the RasPi Camera] -{bayes, svm, multi}" << endl;
 
 }
 
-Mat go(const string &method, const string &model_name, const string &image, const Configuration &config) {
+Mat go(const string &method, const string &model_name, Mat &image, const Configuration &config) {
 
     cout << endl << "---------------" << image << endl;
 
@@ -49,32 +49,12 @@ Mat go(const string &method, const string &model_name, const string &image, cons
 
     cout << "LABELS: " << labels << endl;
 
-    for (int i = 0; i < features.size(); ++i) {
-
-        string folder = results_folder + "/neg/";
-
-        if (labels.at<int>(0, i) > 0 || labels.at<float>(0, i) > 0) {
-            folder = results_folder + "/pos/";
-        }
-
-        imwrite(
-                folder +
-                extractFileName(set_format(image, "", false), "/") +
-                "_L" + to_string(features[i].label) +
-                "_" + to_string(features[i].id) +
-                ".bmp",
-                features[i].candidate
-        );
-    }
-
     return labels;
 }
 
-void classificationPhase(char*argv[], const Configuration &config){
+void classificationPhase(char*argv[], Mat target, const Configuration &config){
 
     auto method = string(argv[2]);
-    auto target_type = string(argv[3]);
-    auto target = string(argv[4]);
     auto model_name = string(argv[5]);
 
     cout << "classify Method: " << method << endl;
@@ -85,17 +65,7 @@ void classificationPhase(char*argv[], const Configuration &config){
 
     /*--------------------------------- classify Phase ------------------------------*/
     int numberOfCandidatesFound = 0;
-    if (target_type == "-d") { /// Whole folder
-
-        vector<string> fn = extractImagePath(target);
-
-        cout << "Number of image found in the directory: " << fn.size() << endl;
-        for (const auto &image : fn) {
-            numberOfCandidatesFound += go(method, model_name, image, config).cols;
-        }
-    } else if (target_type == "-i") { /// Single Image
-        numberOfCandidatesFound += go(method, model_name, target, config).cols;
-    }
+    numberOfCandidatesFound += go(method, model_name, target, config).cols;
 
     cout << "Candidates at first segmentation found: " << numberFirstSPCandidatesFound << endl;
     cout << "Candidates found: " << numberOfCandidatesFound << endl;
@@ -106,11 +76,12 @@ int main(int argc, char *argv[]) {
 //    cout << phd::io::GetCurrentWorkingDir() << endl;
 
     if (argc < 2) {
+        showHelper();
         return 0;
     } else {
 
         auto mode = string(argv[1]);
-
+        auto poison_pill = false;
         config = loadProgramConfiguration(config_folder + "/config.json");
 
         printThresholds(config.primaryThresholds);
@@ -118,7 +89,14 @@ int main(int argc, char *argv[]) {
         printOffsets(config.offsets);
 
         if (mode == "-o" && argc > 2) {
-            classificationPhase(argv, config);
+            while(!poison_pill) {
+                // 1. Fetch Image
+                // 2. Fetch GPS Coordinates
+                //
+                Mat image;
+                classificationPhase(argv, image, config);
+            }
+
         } else {
             showHelper();
         }
