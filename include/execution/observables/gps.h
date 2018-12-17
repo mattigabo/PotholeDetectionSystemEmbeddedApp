58e3a671be6a29b9dde3f42b30d9a15cb349d900
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-12-17 16:01:00 +0100

    RENAMED a variable and adjusted a comment

--- include/execution/observables/gps.h
+++ include/execution/observables/gps.h
@@ -23,14 +23,14 @@
 namespace observables {
     namespace gps {
 
-        const long GPS_REFRESH_RATE = 1500L;
+        const long GPS_REFRESH_PERIOD = 1500L;
 
         /**
         * Create an HOT Observable from the given GPSDataStore. Values are picked every refresh
         *
         * @param gpsDataStore The storage from which the GPS Coordinates are read
         * @param refresh The refresh time of the read
-        * @return An observable of Coordinates
+        * @return An observable stream of Coordinates
         */
         rxcpp::observable<phd::devices::gps::Coordinates>
         createGPSObservable(phd::devices::gps::GPSDataStore *gpsDataStore, const long refresh);
